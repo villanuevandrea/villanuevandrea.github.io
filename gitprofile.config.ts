d13e3fcca07f10e432ec05d6a5a8ab62f9c2f617
villanuevandrea <162624930+villanuevandrea@users.noreply.github.com>
2024-04-11 10:08:27 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -52,13 +52,13 @@ const CONFIG = {
     },
   },
   seo: {
-    title: 'Portfolio of Tricia Van Laar',
+    title: 'Portfolio of Andrea Villanueva',
     description: '',
     imageURL: '',
   },
   social: {
-    linkedin: 'triciavanlaar',
-    twitter: 'tvanlaarmicro',
+    linkedin: '',
+    twitter: '',
     mastodon: '',
     researchGate: '',
     facebook: '',
@@ -71,9 +71,9 @@ const CONFIG = {
     stackoverflow: '', // example: '1/jeff-atwood'
     skype: '',
     telegram: '',
-    website: 'https://tvanlaar.github.io',
+    website: 'https://villanuevandrea.github.io',
     phone: '',
-    email: 'tvanlaar@csustan.edu',
+    email: 'avillanueva18@csustan.edu',
   },
   resume: {
     fileUrl:
